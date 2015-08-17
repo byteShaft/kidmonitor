d34a92c3e83e9a602135c4fb17b0ba9fb1667719
Your Name <hussi@gmail.com>
2015-08-17 13:33:11 +0500

    silent volume when start recording, add call states

--- app/src/main/java/com/byteshaft/kidmonitor/IncomingCallStateListener.java
+++ app/src/main/java/com/byteshaft/kidmonitor/IncomingCallStateListener.java
@@ -4,26 +4,39 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 
 public class IncomingCallStateListener extends PhoneStateListener {
 
     Context mContext;
 
-    BroadcastReceiver mOutgoingCallListener = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            System.out.println("Outgoing");
-        }
-    };
-
-    @Override
-    public void onCallStateChanged(int state, String incomingNumber) {
-        super.onCallStateChanged(state, incomingNumber);
-        System.out.println("Incoming");
-    }
-
     public IncomingCallStateListener(Context context) {
         super();
         mContext = context;
     }
+
+    @Override
+    public void onCallStateChanged(int state, String incomingNumber) {
+        super.onCallStateChanged(state, incomingNumber);
+
+        switch (state) {
+            case TelephonyManager.CALL_STATE_RINGING:
+                System.out.println("RINGING");
+                break;
+            case TelephonyManager.CALL_STATE_IDLE:
+                System.out.println("IDLE");
+                break;
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+                System.out.println("OFFHOOK");
+                break;
+        }
+    }
+
+    BroadcastReceiver mOutgoingCallListener = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String outGoingNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+            System.out.println(outGoingNumber);
+        }
+    };
 }
