db23bbae353589680f95c3308fcf9e5c9fa76fa1
Omer Akram <om26er@gmail.com>
2015-08-21 18:24:39 +0500

    Implement Google Cloud Messaging for remote commands.

--- app/src/main/java/com/byteshaft/kidmonitor/MainActivity.java
+++ app/src/main/java/com/byteshaft/kidmonitor/MainActivity.java
@@ -1,22 +1,35 @@
 package com.byteshaft.kidmonitor;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.byteshaft.kidmonitor.recorders.VideoRecorder;
 import com.byteshaft.kidmonitor.services.CallListenerService;
 import com.byteshaft.kidmonitor.services.LocationService;
+import com.byteshaft.kidmonitor.services.RegistrationIntentService;
 import com.byteshaft.kidmonitor.utils.Helpers;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class MainActivity extends AppCompatActivity {
 
     public LocationService mLocationService;
     private VideoRecorder videoRecorder;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,10 +73,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         startService(new Intent(this, CallListenerService.class));
+
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                System.out.println("Registered");
+            }
+        };
+
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter("registrationComplete"));
     }
 
     @Override
     protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
         super.onPause();
         if (mLocationService != null && mLocationService.mGoogleApiClient.isConnected()) {
             mLocationService.stopLocationService();
