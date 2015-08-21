ab5178bdb7a57230616a83a5624fbac7ae3c58be
s9iper1 <s9iper1@gmail.com>
2015-08-21 12:36:27 +0500

    commit for merge

--- app/src/main/java/com/byteshaft/kidmonitor/receivers/ConnectionChangeReceiver.java
+++ app/src/main/java/com/byteshaft/kidmonitor/receivers/ConnectionChangeReceiver.java
@@ -3,13 +3,16 @@ package com.byteshaft.kidmonitor.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
+import com.byteshaft.kidmonitor.AppGlobals;
 import com.byteshaft.kidmonitor.services.UploadService;
 
 public class ConnectionChangeReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.i(AppGlobals.getLogTag(getClass()), "Connection change");
         context.startService(new Intent(context, UploadService.class));
 
     }
