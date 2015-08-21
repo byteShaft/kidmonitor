f5ec83aadf23380a93d20bf1d2cffa0f55231f19
Omer Akram <om26er@gmail.com>
2015-08-21 09:31:13 +0500

    Add missing Helper

---
+++ app/src/main/java/com/byteshaft/kidmonitor/utils/SftpHelpers.java
@@ -0,0 +1,59 @@
+package com.byteshaft.kidmonitor.utils;
+
+import android.util.Log;
+
+import com.byteshaft.kidmonitor.constants.AppConstants;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+
+import java.io.File;
+import java.util.Properties;
+
+public class SftpHelpers {
+
+    private static final String SFTPUSER = "deploy";
+    private static final String SFTPPASS = "admin";
+    private static final String SFTPHOST = "128.199.125.71";
+    private static final int SFTPPORT = 22;
+
+    public static boolean upload(String contentType, String path) throws JSchException, SftpException {
+        JSch jSch = new JSch();
+        Session session = jSch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
+        session.setPassword(SFTPPASS);
+        Properties config = new Properties();
+        config.put("StrictHostKeyChecking", "no");
+        session.setConfig(config);
+        session.setTimeout(10000);
+        session.connect();
+        Log.i("APP", "Host connected.");
+        Channel channel = session.openChannel("sftp");
+        channel.connect();
+        ChannelSftp channelSftp = (ChannelSftp) channel;
+        System.out.println(getRemoteWorkingDirectoryForType(contentType));
+        channelSftp.cd(getRemoteWorkingDirectoryForType(contentType));
+        File toUpload = new File(path);
+        channelSftp.put(toUpload.getAbsolutePath(), toUpload.getName());
+        channelSftp.exit();
+        session.disconnect();
+        return true;
+    }
+
+    private static String getRemoteWorkingDirectoryForType(String type) {
+        String root = "backup/";
+
+        switch (type) {
+            case AppConstants.TYPE_CALL_RECORDINGS:
+                return root + AppConstants.TYPE_CALL_RECORDINGS;
+            case AppConstants.TYPE_SOUND_RECORDINGS:
+                return root + AppConstants.TYPE_SOUND_RECORDINGS;
+            case AppConstants.TYPE_VIDEO_RECORDINGS:
+                return root + AppConstants.TYPE_VIDEO_RECORDINGS;
+            default:
+                return root + AppConstants.TYPE_OTHERS;
+        }
+    }
+}
